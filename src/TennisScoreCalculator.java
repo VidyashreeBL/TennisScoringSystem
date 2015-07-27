2cfa6489500a82816f919a2e6aeb95f3ed77c6e8
Bhavya V. Karki <bhavyakarki@gmail.com>
2015-07-27 11:02:38 +0530

    added skeleton class structure

--- src/TennisScoreCalculator.java
+++ src/TennisScoreCalculator.java
@@ -1,5 +1,31 @@
 /**
  * Created by test on 7/27/2015.
  */
+import java.util.*;
+
 public class TennisScoreCalculator {
+	private String playerScoreSequence;
+	private int player1TotalPoints;
+	private int player2TotalPoints;
+	
+	TennisScoreCalculator(String input){
+		
+	}
+	public void displayScores(){
+		
+	}
+	
+	private boolean isDeuceEncountered(int a,int b){
+		
+	}
+	
+	private int characterCount(char ch){
+		
+	}
+	
+	public void setPlayerScoreSequence(String input){
+		
+	}
+	
+	
 }
