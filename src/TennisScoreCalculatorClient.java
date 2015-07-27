3799d71985fab0959bfe5eb3c1b23259729288cd
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-27 13:32:01 +0530

    Client class implementation

--- src/TennisScoreCalculatorClient.java
+++ src/TennisScoreCalculatorClient.java
@@ -1,8 +1,9 @@
 
 public class TennisScoreCalculatorClient {
     public static void main(String[] args){
-        System.out.println("here");
-        TennisScoreCalculator tsc = new TennisScoreCalculator("DFDD");
-        tsc.displayScores();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        TennisScoreCalculator tsc = new TennisScoreCalculator(input);
+        tsc.showScoreboard();
     }    
 }
