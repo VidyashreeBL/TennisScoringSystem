68ba81be27ed90a51f463e4213a84ca8ad1a1833
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-27 11:22:29 +0530

    Client class which uses the TennisScoreCalculator class

---
+++ src/TennisScoreCalculatorClient.java
@@ -0,0 +1,8 @@
+
+public class TennisScoreCalculatorClient {
+    public static void main(String[] args){
+        System.out.println("here");
+        TennisScoreCalculator tsc = new TennisScoreCalculator("DFDD");
+        tsc.displayScores();
+    }    
+}
